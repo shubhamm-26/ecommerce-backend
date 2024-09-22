751cb15746bf13b0df13aff4e609323d252fa680
Shubham <202152323@iiitvadodara.ac.in>
2024-09-22 16:21:11 +0530

    Oauth fix

--- middlewares/passport.js
+++ middlewares/passport.js
@@ -5,7 +5,7 @@ const User = require('../models/user');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback'
+  callbackURL: 'https://ecommerce-backend2604.azurewebsites.net/auth/google/callback'
 },
 async (accessToken, refreshToken, profile, done) => {
   const { id, emails, displayName } = profile;
