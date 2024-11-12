9a36d7384659971eef629bc20f9a553103173460
Shubham <202152323@iiitvadodara.ac.in>
2024-11-12 22:01:19 +0530

    rate limiter

--- server.js
+++ server.js
@@ -5,15 +5,24 @@ const session = require('express-session');
 require("./utils/db");
 const cors = require('cors');
 const requestLogger = require('./middlewares/requestLogger');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 app.use(requestLogger);
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests from this IP, please try again later."
+});
+
+app.use(limiter);
 
 app.use(session({
     secret: process.env.SECRET_KEY,
     resave: false,
     saveUninitialized: false
-  }));
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
